c1bb3b1c9cec57fbf86fcc4e046a6311da466394
t0hka <czk1263183073@outlook.com>
2022-04-21 00:00:03 +0800

    fix:Fixed an mistake with conflicting sqlite operations

--- modules/auto_reply/reply.go
+++ modules/auto_reply/reply.go
@@ -8,35 +8,45 @@ import (
 )
 
 func Reply(qqclient *client.QQClient,msg *message.PrivateMessage)  {
-	// 查询数据库
-	// 查询到了就返回
-	if strings.Contains(msg.ToString(),"learn"){
-		return
-	}
-	db,err:=sql.Open("sqlite3","./learn.db")
-	if err!=nil{
-		logger.WithError(err).Error("open db error")
-		return
-	}
-	defer db.Close()
-	rows,err:=db.Query("select answer from learn where keyword=?",msg.ToString())
-	if err!=nil{
-		logger.WithError(err).Error("select error")
-		return
-	}
-	defer rows.Close()
-	for rows.Next(){
-		println("I am here")
-		//var keyword string
-		var answer string
-		err=rows.Scan(&answer)
-		println(err)
-		if err!=nil{
-			logger.WithError(err).Error("scan error")
+	if msg.ToString()!=""{
+		if strings.Contains(msg.ToString(),"learn"){
 			return
 		}
-		println(answer)
-		answer1:=message.NewSendingMessage().Append(message.NewText(answer))
-		qqclient.SendPrivateMessage(msg.Sender.Uin,answer1)
+		db,err:=sql.Open("sqlite3","./learn.db")
+		if err!=nil{
+			logger.WithError(err).Error("open db error")
+			return
+		}
+		defer func(db *sql.DB) {
+			err := db.Close()
+			if err != nil {
+				logger.WithError(err).Error("close db error")
+			}
+		}(db)
+		rows,err:=db.Query("select answer from learn where keyword=?",msg.ToString())
+		if err!=nil{
+			logger.WithError(err).Error("select error")
+			return
+		}
+		defer func(rows *sql.Rows) {
+			err := rows.Close()
+			if err != nil {
+				logger.WithError(err).Error("close rows error")
+			}
+		}(rows)
+		for rows.Next(){
+			println("I am here")
+			//var keyword string
+			var answer string
+			err=rows.Scan(&answer)
+			println(err)
+			if err!=nil{
+				logger.WithError(err).Error("scan error")
+				return
+			}
+			println(answer)
+			answer1:=message.NewSendingMessage().Append(message.NewText(answer))
+			qqclient.SendPrivateMessage(msg.Sender.Uin,answer1)
+		}
 	}
 }
